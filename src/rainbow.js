ff7af32de5b5304a82cbfdceb290b57084b8b5f3
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-02-10 15:38:26 +0100

    Add rainbowText function

--- src/rainbow.js
+++ src/rainbow.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 
-//Rainbow Header
+    //Rainbow Header
     function rainbowHeader() {
 
         var height = $('.rainbow').height(),
@@ -15,21 +15,49 @@ $(document).ready(function () {
                     deg = $(this).data('degrees'),
                     gradient = $('[data-type*="' + type + '"]#' + $(this).attr('id') + '').data('color'),
                     colors = gradient.split('-'),
-                    head = $('.rainbow[data-type="' + type + '"]:before'); 
+                    head = $('.rainbow[data-type="' + type + '"]:before');
 
-              if($(this).data('type') == 'linear'){
-                var deg =  deg + 'deg,'
-              } 
-              else{
-              var deg = ''
-              }  
-               
-                body.prepend('<style>.rainbow#' + $(this).attr('id') + ':before { position: relative; content: ""; display: block;margin:0; width: 100%; height:100%;max-width:'+ width +'px;max-height:'+height+'px;min-width:'+ width +'px;min-height:'+height+'px; ' + height + 'px; opacity:'+ opacity +'; z-index: 1; } .rainbow[data-type="' + type + '"]#' + $(this).attr('id') + ':before{background:' + type + '-gradient('+deg+'#' + colors[0] + ',#' + colors[1] + ')}</style>'); 
-            }); 
+                if ($(this).data('type') == 'linear') {
+                    var deg = deg + 'deg,'
+                } else {
+                    var deg = ''
+                }
+
+                body.prepend('<style>.rainbow#' + $(this).attr('id') + ':before { position: relative; content: ""; display: block;margin:0; width: 100%; height:100%;max-width:' + width + 'px;max-height:' + height + 'px;min-width:' + width + 'px;min-height:' + height + 'px; ' + height + 'px; opacity:' + opacity + '; z-index: 1; } .rainbow[data-type="' + type + '"]#' + $(this).attr('id') + ':before{background:' + type + '-gradient(' + deg + '#' + colors[0] + ',#' + colors[1] + ')}</style>');
+            });
 
 
     }
 
+    function rainbowText() {
+
+        $this = $('body').find('h1.rainbow,h2.rainbow,h3.rainbow,h4.rainbow,h5.rainbow,h6.rainbow,a.rainbow,p.rainbow').each(function () {
+
+            var body = $('body'),
+
+                height = $(this).height(),
+                width = $(this).width(),
+
+                opacity = $(this).data('opacity'),
+                type = $(this).data('type'),
+                deg = $(this).data('degrees'),
+                gradient = $('[data-type*="' + type + '"]#' + $(this).attr('id') + '').data('color'),
+                colors = gradient.split('-'),
+                head = $('.rainbow[data-type="' + type + '"]:before');
+
+            if ($(this).data('type') == 'linear') {
+                var deg = deg + 'deg,'
+            } else {
+                var deg = ''
+            }
+
+            body.prepend('<style>.rainbow#' + $(this).attr('id') + '{height:' + height + 'px;background:' + type + '-gradient(' + deg + '#' + colors[0] + ',#' + colors[1] + '); background-size:120%; -webkit-background-clip:text; background-clip:text; -webkit-text-fill-color:transparent;}.rainbow#' + $(this).attr('id') + ':before{display:none}  </style>');
+        });
+
+    }
+
+
+    rainbowText();
     rainbowHeader();
 
 });
