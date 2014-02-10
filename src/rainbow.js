64b381f6f3cfad0c5f904cd3bc1df6985e2be7d2
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-02-10 14:55:17 +0100

    Update rainbow.js
    Fix size rainbowHeader();

--- src/rainbow.js
+++ src/rainbow.js
@@ -9,20 +9,22 @@ $(document).ready(function () {
 
                 var body = $('body'),
                     height = $(this).height(),
+                    width = $(this).width(),
+                    opacity = $(this).data('opacity'),
                     type = $(this).data('type'),
                     deg = $(this).data('degrees'),
                     gradient = $('[data-type*="' + type + '"]#' + $(this).attr('id') + '').data('color'),
                     colors = gradient.split('-'),
-                    head = $('.rainbow[data-type="' + type + '"]:before');
+                    head = $('.rainbow[data-type="' + type + '"]:before'); 
 
               if($(this).data('type') == 'linear'){
                 var deg =  deg + 'deg,'
               } 
               else{
               var deg = ''
-              }
+              }  
                
-                body.prepend('<style>.rainbow#' + $(this).attr('id') + ':before { position: absolute; content: ""; display: block; width: 100%; height: ' + height + 'px; opacity: .75; z-index: 1; } .rainbow[data-type="' + type + '"]#' + $(this).attr('id') + ':before{background:' + type + '-gradient('+deg+'#' + colors[0] + ',#' + colors[1] + ')}</style>'); 
+                body.prepend('<style>.rainbow#' + $(this).attr('id') + ':before { position: relative; content: ""; display: block;margin:0; width: 100%; height:100%;max-width:'+ width +'px;max-height:'+height+'px;min-width:'+ width +'px;min-height:'+height+'px; ' + height + 'px; opacity:'+ opacity +'; z-index: 1; } .rainbow[data-type="' + type + '"]#' + $(this).attr('id') + ':before{background:' + type + '-gradient('+deg+'#' + colors[0] + ',#' + colors[1] + ')}</style>'); 
             }); 
 
 
