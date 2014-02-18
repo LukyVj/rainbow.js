6db6d51e20efd0230dacfd22ebea92b8915e5ff2
LukyVj <lucas.bonomi@gmail.com>
2014-02-18 13:48:09 +0100

    edit script

--- src/rainbow.js
+++ src/rainbow.js
@@ -26,13 +26,18 @@ $(document).ready(function () {
               }       
           body.prepend('<style id="dynamicStyle">.rainbow#' + $(this).attr('id') + ':before { position: absolute; content: ""; display: block;margin:0; width: 100%; height:100%;max-width:' + width + 'px;max-height:' + height + 'px;min-width:' + width + 'px;min-height:' + height + 'px; ' + height + 'px; opacity:' + opacity + '; z-index: 1; } .rainbow[data-type="' + type + '"]#' + $(this).attr('id') + ':before{background:' + type + '-gradient(' + deg + '#' + colors[0] + ',#' + colors[1] + ')}</style>');   
          
-          $(window).resize(function() {
-            var dyn = document.querySelector('#dynamicStyle'),
-                dynTxt = dyn.innerText,
-                newWidth = document.width,
-                newdynTxt = dynTxt.replace(width, newWidth);
-                console.log('old width :' + width + '\n' + 'New width : ' + newWidth);
-            dyn.innerText = newdynTxt;
+          $(window).resize(function() {                               
+                $this = $('body').find('.rainbow').not('[id*="gradient_"],[id*="rainText"],[id*="rainImage"]').each(function () {
+                var dyn = $('#dynamicStyle'),
+                 dynTxt = dyn.text(),
+                 newWidth = $(this).parent().width(),
+                 newdynTxt = dynTxt.replace(width, newWidth);
+                 $(this).css({width: $(this).width +'px' });
+                 
+                 dyn.text(newdynTxt);
+          
+                });
+               
          });
 
       });
